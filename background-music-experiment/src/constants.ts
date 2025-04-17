410077c1d4ad138cbec1829e248d86438bfc3759
nignig0 <tanitoluwaadebayo@gmail.com>
2025-04-17 10:52:47 +0000

    changing the build script

--- background-music-experiment/src/constants.ts
+++ background-music-experiment/src/constants.ts
@@ -1,2 +1,2 @@
-export const backendUrl  = 'http://127.0.0.1:5000'
+export const backendUrl  = 'https://background-music-experiment.onrender.com'
 export const spotifyBaseUrl = 'https://api.spotify.com/v1/playlists'
